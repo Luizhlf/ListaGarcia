9459465654b3d69fe8b2f1a26696be6fca6eaa1f
Luizhlf <luiz301202lh@gmail.com>
2025-11-05 11:51:50 -0300

    exercícios

--- exercicio5.js
+++ exercicio5.js
@@ -1,13 +1,11 @@
-function alunosNotas(notas){
-  const ul = document.createElement("ul")
-  notas.filter((nota)=>{
-    if(nota.nota >= 7){
-      return nota
-    }
-  }).map((nota)=>{
-    const li = document.createElement("li")
-    li.textContent = `${nota.nome} ${nota.nota}`
-    ul.appendChild(li)
-  })
-  document.body.appendChild(ul)
+function alunosNotas(notas) {
+  const ul = document.createElement("ul");
+
+  notas.filter(nota => nota.nota >= 7).forEach(nota => {
+      const li = document.createElement("li");
+      li.textContent = `${nota.nome} ${nota.nota}`;
+      ul.appendChild(li);
+    });
+
+  document.body.appendChild(ul);
 }
--- exercicio6.js
+++ exercicio6.js
@@ -1,14 +1,12 @@
-function estoqueTotal(estoque){
-  let p = document.createElement("p");
+function estoqueTotal(estoque) {
+  const p = document.createElement("p");
 
-  const produtosEstoque = estoque.filter((quantidade)=>{
-    if(quantidade.quantidade > 5){
-      return quantidade
-    }
-  })
-  const valorProdutos = produtosEstoque.reduce((total, produto)=>{
-    return total + (produto.preco * produto.quantidade)
-  }, 0)
-  p.textContent = `Valor total do estoque (produtos com mais de 5 unidades): R$ ${valorProdutos}`
-  document.body.appendChild(p)
+  const produtosEstoque = estoque.filter(produto => produto.quantidade > 5);
+
+  const valorProdutos = produtosEstoque.reduce((total, produto) => {
+    return total + (produto.preco * produto.quantidade);
+  }, 0);
+
+  p.textContent = `Valor total do estoque (produtos com mais de 5 unidades): R$ ${valorProdutos}`;
+  document.body.appendChild(p);
 }
